78163fc14e7c7c8b25b0ac05f2e770851b4834f3
Max <jmksm96@gmail.com>
2021-06-09 21:00:06 +0300

    Init Release

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            This is APP component
+            <Rating/>
+        </div>
+    );
 }
 
 export default App;
+
+
+const Rating = () => {
+    return (
+        <div>
+            <div> start</div>
+            <div> start</div>
+            <div> start</div>
+            <div> start</div>
+            <div> start</div>
+        </div>
+    );
+};
+
+const Accordion = () => {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
+};
+
